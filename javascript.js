3342e18f0f72922b65f7c0b22dcf8c22c7a5acfc
HieuTV18 <71161933+HieuTV18@users.noreply.github.com>
2021-05-27 10:54:19 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -66,21 +66,52 @@ document.getElementById("help").onclick = function(){
 }
 
 // slide changeIMG 
-var index = 1;
-changeImage =function() {
-    var imgs = [
-    "./image/slideshow/IMG_0249.JPG",
-    "./image/slideshow/IMG_0251.JPG",
-    "./image/slideshow/IMG_0877.JPG"
-]
-    document.getElementById("img").src = imgs[index];
-    index++;
-    if(index == 3){
-        index =0;
+// var index = 1;
+// changeImage =function() {
+//     var imgs = [
+//     "./image/slideshow/IMG_0249.JPG",
+//     "./image/slideshow/IMG_0251.JPG",
+//     "./image/slideshow/IMG_0877.JPG"
+// ]
+//     document.getElementById("img").src = imgs[index];
+//     index++;
+//     if(index == 3){
+//         index =0;
+//     }
+// }
+// setInterval(changeImage,8000)
+
+window.onscroll = function(){
+    // console.info(document.documentElement.scrollTop);
+    var gototop = document.getElementById('gototop');
+    if (document.documentElement.scrollTop > 500 || document.body.scrollTop > 500){
+        gototop.style.display = "flex";
+    }
+    else{
+        gototop.style.display = "none";
     }
 }
-setInterval(changeImage,8000)
+function goToTop(){
+    var timer = setInterval(function(){
+        document.documentElement.scrollTop-=20;
+        if(document.documentElement.scrollTop<=0)
+        clearInterval(timer);
+    },1)
+}
 
-// marquee 
-// onmouseover= "document.getElementById('marquee-two').stop();"
-// onmouseout="document.getElementById('marquee-two').start();"
+// let ListBtnHelp = document.getElementsByClassName("buy-product_btn");
+// console.log(ListBtnHelp)
+// for (let i =0; i<ListBtnHelp.length;i++){
+//     ListBtnHelp[i].onclick = function(){
+//         document.getElementById("methodSupport").style.display = "flex";
+//     }
+// }
+
+function ShowMethodSupport(){
+        document.getElementById("methodSupport").style.display = "flex";
+
+}
+function HiddenMethodSupport(){
+    document.getElementById("methodSupport").style.display = "none";
+
+}
